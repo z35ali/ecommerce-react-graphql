e891a05ea358ae5a82a50a45a24efc42725a3823
Zafar <zafar.a.ali2@gmail.com>
2020-01-10 17:10:13 -0500

    Add markup for brands section in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { Container, Box, Heading } from 'gestalt';
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <Container>
+        {/* Brands Section */}
+        <Box display='flex' justifyContent='center' marginBottom={2}>
+          {/* Brands Header */}
+          <Heading color='midnight' size='md'>
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
+    );
   }
 }
 
