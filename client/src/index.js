a9232592c83f5b3132a444e8048883465d46ad7f
Zafar <zafar.a.ali2@gmail.com>
2020-01-10 15:26:51 -0500

    Create routes for different components

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import Signin from './components/Signin';
+import Checkout from './components/Checkout';
+import Signup from './components/Signup';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route component={App} exact path='/' />
+      <Route component={Signin} path='/signin' />
+      <Route component={Signup} path='/signup' />
+      <Route component={Checkout} path='/checkout' />
+    </Switch>
+  </Router>
+);
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
