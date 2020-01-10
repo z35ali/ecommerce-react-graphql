7a33308362d81144d0ae621eaf3a453ddd9e1e24
Zafar <zafar.a.ali2@gmail.com>
2020-01-10 17:50:09 -0500

    Mobile responsiveness for brands
    - flex wrap

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -49,13 +49,24 @@ class App extends Component {
           </Heading>
         </Box>
         {/* Brands */}
-        <Box display='flex' justifyContent='around'>
+        <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: '#d6c8ec'
+            }
+          }}
+          shape='rounded'
+          wrap
+          display='flex'
+          justifyContent='around'
+        >
           {brands.map(brand => (
-            <Box margin={2} width={200} key={brand._id}>
+            <Box paddingY={4} margin={2} width={200} key={brand._id}>
               <Card
                 image={
                   <Box height={200} width={200}>
                     <Image
+                      fit='cover'
                       alt='Brand'
                       naturalHeight={1}
                       naturalWidth={1}
