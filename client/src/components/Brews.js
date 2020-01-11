d5ac7d66e8eeca0944de556998a0f33ba13654ab
Zafar <zafar.a.ali2@gmail.com>
2020-01-10 22:47:11 -0500

    Display brews for each brand in React front end
    - Loading spinner also

--- client/src/components/Brews.js
+++ client/src/components/Brews.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Strapi from 'strapi-sdk-javascript/build/main';
+import { Box, Heading, Button, Text, Card, Image, Container } from 'gestalt';
+import Loader from './Loader';
 const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
 export default class Brews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brews: [],
+      brand: '',
+      loadingBrews: true
+    };
+  }
   async componentDidMount() {
     try {
-      const response = await strapi.request('POST', '/graphql', {
+      const { data } = await strapi.request('POST', '/graphql', {
         data: {
           query: `query{
             brand(id: "${this.props.match.params.brandId}"){
@@ -25,13 +35,82 @@ export default class Brews extends Component {
          }`
         }
       });
-      console.log(response);
+      this.setState({ brews: data.brand.brews, brand: data.brand.name });
     } catch (err) {
       console.error(err);
     }
+    this.setState({ loadingBrews: false });
   }
 
   render() {
-    return <div></div>;
+    const { brand, brews, loadingBrews } = this.state;
+    return (
+      <Container>
+        <Box
+          marginTop={4}
+          display='flex'
+          justifyContent='center'
+          alignItems='start'
+        >
+          {/* Brews Section */}
+          <Box display='flex' direction='column' alignItems='center'>
+            {/* Brews Heading */}
+            <Box margin={2}>
+              <Heading color='orchid'>{brand}</Heading>
+            </Box>
+            {/* Brews */}
+            <Box
+              dangerouslySetInlineStyle={{
+                __style: {
+                  backgroundColor: '#bdcdd9'
+                }
+              }}
+              shape='rounded'
+              display='flex'
+              wrap
+              justifyContent='center'
+              padding={4}
+            >
+              {brews.map(brew => (
+                <Box paddingY={4} margin={2} width={210} key={brew._id}>
+                  <Card
+                    image={
+                      <Box height={250} width={200}>
+                        <Image
+                          fit='cover'
+                          alt='Brand'
+                          naturalHeight={1}
+                          naturalWidth={1}
+                          src={`${apiUrl}${brew.image.url}`}
+                        />
+                      </Box>
+                    }
+                  >
+                    <Box
+                      display='flex'
+                      alignItems='center'
+                      justifyContent='center'
+                      direction='column'
+                    >
+                      <Box marginBottom={2}>
+                        <Text size='xl'>{brew.name}</Text>
+                      </Box>
+                      <Text size='xl'>{brew.description}</Text>
+                      <Text color='orchid'>${brew.price}</Text>
+                      <Box marginTop={2}>
+                        <Text size='xl'>
+                          <Button color='blue' text='Add to Cart' />
+                        </Text>
+                      </Box>
+                    </Box>
+                  </Card>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+        <Loader show={loadingBrews} />
+      </Container>
+    );
   }
 }
